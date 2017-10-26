c10ca8f068fa83aef35a220eb3aba5e65643f491
Ranjith Venkatesh <Ranjith.Venkatesh@mossandlichens.com>
2017-10-26 21:33:29 +0200

    Fixing main fns

--- index.js
+++ index.js
@@ -1,69 +1,51 @@
 "use strict";
 
-exports.b2bOptic = function(callback) {    
+exports.b2bOptic = function() {    
     var files = require('./helpers/files');
-    files.loadSchemaJson(function(err, data){
-        if(err){
-            throw callback(err);
-        }
-        return callback(null, JSON.parse(data));
-    });
-    
+    var schemaJson = files.loadSchemaJson();
+    return JSON.parse(schemaJson);    
 };
 
-exports.validate = function(xmlValue, callback) {    
+exports.validate = function(xmlValue) {    
     var files = require('./helpers/files');
-    files.loadSchema(function(err, data){
-        if(err){
-            throw callback(err);
-        } 
-        var libxmljs = require('libxmljs');
-        var xsdDoc = libxmljs.parseXmlString(data);    
-        var xmlDoc = libxmljs.parseXmlString(xmlValue);    
-        return callback(null, xmlDoc.validate(xsdDoc));    
-    });
+    var schema = files.loadSchema();
+    var libxmljs = require('libxmljs');
+    var xsdDoc = libxmljs.parseXmlString(schema);    
+    var xmlDoc = libxmljs.parseXmlString(xmlValue);    
+    return xmlDoc.validate(xsdDoc);    
 };
 
-exports.deserialize = function(xml, callback) {
+exports.deserialize = function(xml) {
     var xml2js = require('xml2js');
     var parseString = xml2js.parseString;    
-    this.validate(xml, function(err, data){
-        if(err) {
-            throw callback(err);
-        }
-        var xmlValid = data;
-        var jsonObject = null;
-        if(xmlValid) {
-            parseString(xml, function(err,result){
-                if(err) {
-                    throw callback(err);
-                }
-                jsonObject = result;
-            });
-            return callback(null, jsonObject);
-        } else {
-            throw callback(Error("Xml not valid as per schema."));
-        }
-    });    
+    var xmlValid = this.validate(xml);
+    var jsonObject = null;
+    if(xmlValid) {
+        parseString(xml, function(err,result){
+            if(err) {
+                throw err;
+            }
+            jsonObject = result;
+        });
+        return jsonObject;
+    } else {
+        throw Error("Xml not valid as per schema.");
+    }        
 };
 
-exports.serialize = function(b2bOpticObject, callback) {    
+exports.serialize = function(b2bOpticObject) {    
     var xml2js = require('xml2js');
     var builder = new xml2js.Builder();
     var b2bOpticXml = builder.buildObject(b2bOpticObject);
-    this.validate(b2bOpticXml, function(err, data){
-        if(err){
-            callback(err);
-        }
-        var xmlValid = data;
-        if(xmlValid)
-        {
-            return callback(null, b2bOpticXml);
-        }
-        else
-        {
-            return callback("Invalid b2bOptic object");
-        }
-    });
+    var xmlValid = this.validate(b2bOpticXml);
     
+    if(xmlValid)
+    {
+        return b2bOpticXml;
+    }
+    else
+    {
+        return "Invalid b2bOptic object";
+    }
+        
 };
